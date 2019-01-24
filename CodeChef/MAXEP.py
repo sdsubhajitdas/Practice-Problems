0521cb29b98e802163637817ac6acdd2a4ee0bd4
Subhajit Das <jeetudas95@gmail.com>
2019-01-25 01:22:36 +0530

    MAXEP updated

--- CodeChef/MAXEP.py
+++ CodeChef/MAXEP.py
@@ -3,48 +3,48 @@
 	Author - Subhajit Das
 	University of Engineering and Management, Kolkata
     13/12/2018
-    Status - Partial
 '''
 
 from sys import stdout
 
 
+def printf(msg, ans=''):
+    print(msg, ans)
+    stdout.flush()
+
+
+def dec_skip(step):
+    return max(1, step//10)
+
+
 def main():
     n, c = list(map(int, input().strip().split()))
-    high, low = n, 1
-    tc, x = 1000, 0
-
-    while tc > 0 and high > low:
-        mid_low = low + (high-low)//3
-        mid_high = high - (high-low)//3
-
-        tc -= 1
-        print("1", mid_high)
-        stdout.flush()
-        result = int(input())
-        if result == 0:
-            low = mid_high + 1
+    coins = 1000
+    step = max(1, n//100)
+    start, end = 0, n
+    while c != 0:
+        start += step
+        if start >= end:
+            if step == 1:
+                printf(3, start)
+                break
+            start -= step
+            step = dec_skip(step)
             continue
-        elif result == 1:
-            high = mid_high - 1
-            tc -= c
-            print("2")
-            stdout.flush()
 
-        tc -= 1
-        print("1", mid_low)
-        stdout.flush()
-        result = int(input())
-        if result == 0:
-            low = mid_low + 1
-            continue
-        elif result == 1:
-            high = mid_low - 1
-            tc -= c
-            print("2")
-            stdout.flush()
-
-    print("3", low)
+        printf(1, start)
+        res = int(input())
+        if res == 0:
+            coins -= 1
+        elif res == 1:
+            if step == 1:
+                printf(3, start)
+                break
+            printf(2)
+            coins -= c
+            end = min(start, end)
+            start -= step
+            step = dec_skip(step)
 
 
 if __name__ == '__main__':
