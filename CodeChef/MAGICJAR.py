009ed546f26480473e3b17eda45703c0583b6725
Subhajit Das <jeetudas95@gmail.com>
2019-02-24 16:25:28 +0530

    MAGICJAR added

---
+++ CodeChef/MAGICJAR.py
@@ -0,0 +1,22 @@
+# https://www.codechef.com/FEB19B/problems/MAGICJAR
+'''
+    Author - Subhajit Das
+    University of Engineering and Management, Kolkata
+    5/2/2018
+'''
+
+
+def main():
+    for _ in range(int(input())):
+        n = int(input())
+        arr = list(map(int, input().split()))
+
+        count = 0
+        for i in arr:
+            count += (i-1)
+
+        print(count+1)
+
+
+if __name__ == "__main__":
+    main()
