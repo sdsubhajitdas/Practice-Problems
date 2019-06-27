f44efac3ba42cbf76fe9a4158f7d153d0b440c6f
Subhajit Das <jeetudas95@gmail.com>
2019-06-27 17:17:31 +0530

    binary_tree_height added

---
+++ HackerRank/binary_tree_height.py
@@ -0,0 +1,79 @@
+# https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=trees
+'''
+    Author - Subhajit Das
+    University of Engineering and Management, Kolkata
+    27/06/2019
+'''
+
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+
+def traverse(root, count) -> int:
+    if(root == None):
+        return count
+    count += 1
+    return max(traverse(root.left, count), traverse(root.right, count))
+
+
+def height(root) -> int:
+    return traverse(root, -1)
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
