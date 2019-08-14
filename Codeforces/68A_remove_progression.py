d0efa030758116652bc61d65ec4a7af9906aa0e2
Subhajit Das <jeetudas95@gmail.com>
2019-08-14 18:35:42 +0530

    remove_progression added

---
+++ Codeforces/68A_remove_progression.py
@@ -0,0 +1,16 @@
+# http://codeforces.com/contest/1194/problem/A
+'''
+    Author - Subhajit Das
+    University of Engineering and Management, Kolkata
+    14/08/2019
+'''
+
+
+def main():
+    for _ in range(int(input())):
+        n, x = map(int, input().split())
+        print(2*x)
+
+
+if __name__ == "__main__":
+    main()
