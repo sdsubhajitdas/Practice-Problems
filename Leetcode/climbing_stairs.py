acc6a59a180de790de5136717e0fcaa79c58e9ec
Subhajit Das <jeetudas95@gmail.com>
2019-10-22 01:52:36 +0530

    climbing_stairs improved

--- Leetcode/climbing_stairs.py
+++ Leetcode/climbing_stairs.py
@@ -2,23 +2,27 @@
 '''
     Author - Subhajit Das
     University of Engineering and Management, Kolkata
-    10/07/2019
+    22/10/2019
 '''
 
 
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if(n == 0):
-            return 1
-
-        if(n == 1 or n == 2):
-            return n
-
-        result = dict()
-        result[1] = 1
-        result[2] = 2
-
-        for way in range(3, n+1):
-            result[way] = result[way-1] + result[way-2]
-
-        return result[n]
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if(n==0 or n==1): return n
+        
+        cache = [-1]*(n+1)
+        cache[0], cache[1], cache[2] = 0, 1, 2
+        
+        return self.climbStairsHelper(n, cache)
+    
+    def climbStairsHelper(self, step, cache):
+        if(step<=0): return 0
+        
+        if(cache[step] != -1): return cache[step]
+        
+        cache[step] = self.climbStairsHelper(step-1, cache) + self.climbStairsHelper(step-2, cache)
+        return cache[step]
