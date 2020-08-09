4c901025469fa042f60e751285e96719a58a851f
Subhajit Das <jeetudas95@gmail.com>
2020-08-09 18:27:45 +0530

    TEST added

---
+++ CodeChef/TEST.py
@@ -0,0 +1,15 @@
+# https://www.codechef.com/LRNDSA01/problems/TEST
+'''
+    Author @Subhajit Das (sdsubhajitdas.github.io)
+    SWE @Turbot HQ India PVT Ltd.
+    09/08/2020
+'''
+
+def main():
+    n = int(input())
+    while(n!= 42):
+        print(n)
+        n = int(input())
+
+if __name__ == "__main__":
+    main()
