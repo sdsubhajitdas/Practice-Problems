c5e77885e08795a9025b406f60e3265108fb66d4
Subhajit Das <sdsubhajitdas98@gmail.com>
2021-07-04 21:22:06 +0530

    Longest Common Subsequence added

---
+++ Leetcode/LongestCommonSubsequence.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/longest-common-subsequence/
+/*
+    Author @Subhajit Das (sdsubhajitdas.github.io)
+    SWE @Turbot HQ India PVT Ltd.
+    04/07/2021
+*/
+
+import java.util.*;
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int dp[][] = new int[text1.length()+1][text2.length()+1];
+
+
+        for(int i=1; i<=text1.length(); i++) {
+            for(int j=1; j<=text2.length(); j++) {
+                int char1 = text1.charAt(i-1);
+                int char2 = text2.charAt(j-1);
+
+                if(char1 == char2) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[text1.length()][text2.length()];
+    }
+}
