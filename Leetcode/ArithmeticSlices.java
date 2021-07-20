96c4c1c426c68ac659fab574ded0ca9747caa7a4
Subhajit Das <sdsubhajitdas98@gmail.com>
2021-07-20 10:37:32 +0530

    Arithmetic Slices added

---
+++ Leetcode/ArithmeticSlices.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        int[] dp = new int[A.length];
+        int sum = 0;
+        for (int i = 2; i < dp.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                dp[i] = 1 + dp[i - 1];
+                sum += dp[i];
+            }
+        }
+        return sum;
+    }
+}
